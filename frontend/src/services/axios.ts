c4a3209f821af19334b6a4bed7f8a8f719f57eaf
suryakamal03 <professionalsurya3@gmail.com>
2026-02-19 17:57:29 +0530

    render url updated

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://projectmanagement-2-7pb6.onrender.com/api",
 });
 
 axiosInstance.interceptors.request.use(
